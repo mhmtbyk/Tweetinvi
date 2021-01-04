3d09444151c27a07911f568233254fd21a5591c3
mhmtbyk <48418006+mhmtbyk@users.noreply.github.com>
2021-01-04 21:27:09 +0300

    Tweetinvi program added.

---
+++ Tweetinvi/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace TwitterApi2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(928, 407);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "label1";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(960, 55);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(299, 28);
+            this.button6.TabIndex = 21;
+            this.button6.Text = "Friends";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(653, 55);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(299, 28);
+            this.button5.TabIndex = 20;
+            this.button5.Text = "Followers";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(39, 443);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(267, 28);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(39, 407);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(267, 28);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Tweetle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(314, 407);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(606, 64);
+            this.textBox1.TabIndex = 17;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(653, 91);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(606, 308);
+            this.listBox2.TabIndex = 15;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(39, 91);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(606, 308);
+            this.listBox1.TabIndex = 14;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(346, 55);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(299, 28);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Kendi Tweetlerim";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(299, 28);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Anasayfa Tweetleri";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1287, 609);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Tweetinvi/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tweetinvi;
+using Tweetinvi.Parameters;
+using Tweetinvi.Models;
+using Tweetinvi.Controllers;
+using Tweetinvi.Json;
+using Tweetinvi.Streams.Model;
+using Tweetinvi.Public;
+using Tweetinvi.Models.DTO.QueryDTO;
+
+namespace TwitterApi2
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            Auth.SetUserCredentials("eFlPiYpQXPdn23BQ3Iy2C6XLx", "uEdnUNGkFyqSMHRyhnaz89zYa7UsqJdhdrx5rkPvNKAnlLfACS", "494252514-y9BEgi7W4NfA9xszjmWWW3BWDDTiG898Tn1JZMmI", "76lp0c5Jida6btaXFcc5JNRTMIbhQlaCwOXfLVgSUcyy0");
+        }     
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            var homeTimelineParameter = new HomeTimelineParameters
+            {
+                MaximumNumberOfTweetsToRetrieve = 100,
+            };
+            var tweets = Timeline.GetHomeTimeline(homeTimelineParameter);
+            foreach(var item in tweets)
+            {
+                listBox1.Items.Add(item.Text);
+                listBox2.Items.Add(item.Id);
+            } 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            var userTimelineParameters = new UserTimelineParameters();
+            var tweets = Timeline.GetUserTimeline("Tolga_Cbn", userTimelineParameters);
+  
+            foreach (var item in tweets)
+            {
+                listBox1.Items.Add(item.Text);
+                listBox2.Items.Add(item.Id);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                var text = textBox1.Text;
+                var tweets = Tweet.PublishTweet(text);
+                label1.Text = "İşlem Başarılı";
+            }
+            catch
+            {
+                label1.Text = "İşlem Başarısız";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = String.Empty;
+            listBox2.SelectedIndex = listBox1.SelectedIndex;
+            
+            try
+            {
+                var success = Tweet.DestroyTweet(long.Parse(listBox2.SelectedItem.ToString()));
+                label1.Text="Tweet silindi";
+                
+            }
+            catch
+            {
+                label1.Text = "İşlem Başarısız";
+            }
+            
+        }
+        
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+            var user = User.GetUserFromScreenName("Tolga_Cbn");
+            
+            var followerIds = user.GetFollowerIds(Int32.MaxValue);
+            var follower = User.GetFollowers("Tolga_Cbn");
+
+            foreach (var item in followerIds)
+            {
+                listBox2.Items.Add(item);
+            }
+            foreach (var item in follower)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+            RateLimit.RateLimitTrackerMode = RateLimitTrackerMode.TrackAndAwait;
+            var user = User.GetUserFromScreenName("Tolga_Cbn");
+
+            var friendsIds = user.GetFriendIds(Int32.MaxValue);
+            var friends = User.GetFriends("Tolga_Cbn");
+
+            foreach (var item in friendsIds)
+            {
+                listBox2.Items.Add(item);
+            }
+            foreach (var item in friends)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+    }
+}
